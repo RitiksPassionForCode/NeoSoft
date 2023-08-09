7e8a0419ac7f51c76e0618b25cbd02b6c022a28b
RitiksPassionForCode <ritz1196@gmail.com>
2023-08-09 19:04:22 +0530

    LeetCode Problems 09-08023

---
+++ LeetCode/src/com/leetcode/neosoft/BuySellStock.java
@@ -0,0 +1,27 @@
+package com.leetcode.neosoft;
+
+public class BuySellStock {
+
+	public static int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int overallProfit = 0;
+        int profit = 0;
+        
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }
+            profit = prices[i] - min;
+            if(overallProfit < profit){
+                overallProfit = profit;
+            }
+        }
+        return overallProfit;
+    }
+	
+	public static void main(String [] args) {
+		int [] prices = new int [] {2,1,5,3,8,4};
+		int maxProfit = maxProfit(prices);
+		System.out.println(maxProfit);
+	}
+}
---
+++ LeetCode/src/com/leetcode/neosoft/ValidParanthesis.java
@@ -0,0 +1,35 @@
+package com.leetcode.neosoft;
+
+import java.util.Stack;
+
+public class ValidParanthesis {
+	
+	public static boolean isValid(String s) {
+	       Stack<Character> stack = new Stack<>();
+	       for(Character c: s.toCharArray()){
+	           if(c == '('){
+	               stack.push(')');
+	           }
+
+	           else if(c == '{'){
+	               stack.push('}');
+	           }
+
+	           else if(c == '['){
+	               stack.push(']');
+	           }
+
+	           else if (stack.empty() ||  stack.pop() != c){
+	               return false;
+	           }
+	       }
+	       return stack.isEmpty();
+	    }
+	
+	public static void main(String [] args) {
+		String s = "{}[]()";
+		boolean valid = isValid(s);
+		System.out.println(valid);
+	}
+
+}
