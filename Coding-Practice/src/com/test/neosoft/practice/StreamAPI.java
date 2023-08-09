f79111dab58ae968db0ddac6752e9ded668eb626
RitiksPassionForCode <ritz1196@gmail.com>
2023-08-09 19:05:29 +0530

    Stream API Practice 09-08-23

--- Coding-Practice/src/com/test/neosoft/practice/StreamAPI.java
+++ Coding-Practice/src/com/test/neosoft/practice/StreamAPI.java
@@ -14,41 +14,62 @@ public class StreamAPI {
 
 	public static void main(String[] args) {
 
-		List<Integer> list = Arrays.asList(10, 15, 8, 49, 25, 98, 32);
+//		List<Integer> list = Arrays.asList(10, 15, 8, 49, 25, 98, 32);
+//
+//		// Find the list of even no's from given list
+//		list.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+//
+//		// Given a list of integers, find out all the numbers starting with 1
+//		list.stream().map(s -> s + "") // Convert integer to String
+//				.filter(s -> s.startsWith("1")).forEach(System.out::println);
+//
+//		// find duplicate elements in a given integers list in java
+//		Set<Integer> set = new HashSet<>();
+//		list.stream().filter(n -> !set.add(n)).forEach(System.out::println);
+//
+//		// Given the list of integers, find the first element of the list
+//		list.stream().findFirst().ifPresent(System.out::println);
+//
+//		// Given a list of integers, find the total number of elements present in the
+//		// list
+//		long count = list.stream().count();
+//		System.out.println(count);
+//
+//		// find the maximum value element present
+//		list.stream().max(Integer::compare).get();
+//
+//		// Given a String, find the first non-repeated character
+//		String input = "Java Hungry Blog Alive is Awesome";
+//
+//		Character result = input.chars() // Stream of String
+//				// First convert to Character object and then to lowercase
+//				.mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
+//				// Store count
+//				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+//				.entrySet().stream().filter(entry -> entry.getValue() == 1L).map(entry -> entry.getKey()).findFirst()
+//				.get();
+//		System.out.println(result);
+//		
+//		//Given a String, find the first repeated character in it
+//		 String input = "Java Hungry Blog Alive is Awesome";
+//
+//         Character result = input.chars() // Stream of String       
+//                                 .mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s))) // First convert to Character object and then to lowercase         
+//                                 .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())) //Store the chars in map with count 
+//                                 .entrySet()
+//                                 .stream()
+//                                 .filter(entry -> entry.getValue() > 1L)
+//                                 .map(entry -> entry.getKey())
+//                                 .findFirst()
+//                                 .get();
+//			System.out.println(result);
+//		
+		//Given a list of integers, sort all the values present in it 
+		 List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
 
-		// Find the list of even no's from given list
-		list.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
-
-		// Given a list of integers, find out all the numbers starting with 1
-		list.stream().map(s -> s + "") // Convert integer to String
-				.filter(s -> s.startsWith("1")).forEach(System.out::println);
-
-		// find duplicate elements in a given integers list in java
-		Set<Integer> set = new HashSet<>();
-		list.stream().filter(n -> !set.add(n)).forEach(System.out::println);
-
-		// Given the list of integers, find the first element of the list
-		list.stream().findFirst().ifPresent(System.out::println);
-
-		// Given a list of integers, find the total number of elements present in the
-		// list
-		long count = list.stream().count();
-		System.out.println(count);
-
-		// find the maximum value element present
-		list.stream().max(Integer::compare).get();
-
-		// Given a String, find the first non-repeated character
-		String input = "Java Hungry Blog Alive is Awesome";
-
-		Character result = input.chars() // Stream of String
-				// First convert to Character object and then to lowercase
-				.mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
-				// Store count
-				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
-				.entrySet().stream().filter(entry -> entry.getValue() == 1L).map(entry -> entry.getKey()).findFirst()
-				.get();
-		System.out.println(result);
-	}
+         myList.stream()
+               .sorted()
+               .forEach(System.out::println);
+		}
 
 }
