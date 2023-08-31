fd04235439ea5b25a61c578a664ebb00918cbe17
RitiksPassionForCode <ritz1196@gmail.com>
2023-08-31 17:39:08 +0530

    Leetcode practice 31-08-23

---
+++ LeetCode/src/com/leetcode/neosoft/PascalsTriangle.java
@@ -0,0 +1,31 @@
+package com.leetcode.neosoft;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalsTriangle {
+	
+	public static List<List<Integer>> generate(int numRows) {
+	    List<List<Integer>> ans = new ArrayList<>();
+
+	    for (int i = 0; i < numRows; ++i) {
+	      Integer[] temp = new Integer[i + 1];
+	      Arrays.fill(temp, 1);
+	      ans.add(Arrays.asList(temp));
+	    }
+
+	    for (int i = 2; i < numRows; ++i)
+	      for (int j = 1; j < ans.get(i).size() - 1; ++j)
+	        ans.get(i).set(j, ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+
+	    return ans;
+	  }
+
+	public static void main(String[] args) {
+		List<List<Integer>> generate = generate(5);
+		System.out.println(generate);
+
+	}
+
+}
