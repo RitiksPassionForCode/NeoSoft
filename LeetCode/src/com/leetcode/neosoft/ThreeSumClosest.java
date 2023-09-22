622365426ee61d7da42ec74197d5115f0039eb56
RitiksPassionForCode <ritz1196@gmail.com>
2023-09-22 18:01:43 +0530

    LeetCode practice 22-09-23

---
+++ LeetCode/src/com/leetcode/neosoft/ThreeSumClosest.java
@@ -0,0 +1,41 @@
+package com.leetcode.neosoft;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+	
+	//-1 + 2 + 1 = 2
+	 public static int threeSumClosest(int[] nums, int target) {
+	        Arrays.sort(nums);
+	        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum with the sum of the first three elements
+
+	        for (int i = 0; i < nums.length - 2; i++) {
+	            int j = i + 1;
+	            int k = nums.length - 1;
+
+	            while (j < k) {
+	                int sum = nums[i] + nums[j] + nums[k];
+
+	                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+	                    closestSum = sum; // Update closest sum if the current sum is closer to the target
+	                }
+
+	                if (sum < target) {
+	                    j++; // Increment j to increase the sum
+	                } else {
+	                    k--; // Decrement k to decrease the sum
+	                }
+	            }
+	        }
+
+	        return closestSum;
+	    }
+
+	public static void main(String[] args) {
+		int [] nums = new int[] {-1,2,1,-4};
+		int target = 1;
+		int threeSumClosest = threeSumClosest(nums, target);
+		System.out.println(threeSumClosest);
+	}
+
+}
